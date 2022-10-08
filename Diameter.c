c6e2e7652cde85a333c7dc80473fef57ff893336
Banke bihari agrawal <52813916+bankebihariagrawal@users.noreply.github.com>
2022-10-08 11:43:31 +0530

    update diameter.c

--- Diameter.c
+++ Diameter.c
@@ -51,11 +51,20 @@ int main()
 { 
 struct node *root = newNode(1); 
 root->left	 = newNode(2); 
-root->right	 = newNode(3); 
+root->right	// Your First C++ Program
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
+ = newNode(3); 
 root->left->left = newNode(4); 
 root->left->right = newNode(5); 
 
-printf("Diameter of the given binary tree is %d\n", diameter(root)); 
+printf("Diameter of the given binary tree is:  %d\n", diameter(root)); 
 
 return 0; 
 } 
+
