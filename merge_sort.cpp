a069a96d0add79958016b227b693ebbd5c171ed8
Banke bihari agrawal <52813916+bankebihariagrawal@users.noreply.github.com>
2022-10-08 17:29:25 +0530

    UPDATE MERGE.CPP
    I UPDATED YOUR OLD TO LATEST CODE WITH RECURSION TECHNIQUE

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,35 +1,92 @@
-#include <bits/stdc++.h>
+#include <iostream>
+ 
 using namespace std;
-void merge_sort(int arr[], int low, int mid, int high)
+ 
+// A function to merge the two half into a sorted data.
+void Merge(int *a, int low, int high, int mid)
 {
-    int i, j, k;
-    int arr_temp[100];
-
-    while (i <= mid && j <= high)
-    {
-        if (arr[i] < arr[j])
-            arr_temp[k++] = arr[i++];
-        else
-            arr_temp[k++] = arr[j++];
-    }
-
-    for (; i <= mid; i++)
-    {
-        arr_temp[k++] = arr[i];
-    }
-    for (; j <= high; j++)
-    {
-        arr_temp[k++] = arr[j];
-    }
+	// We have low to mid and mid+1 to high already sorted.
+	int i, j, k, temp[high-low+1];
+	i = low;
+	k = 0;
+	j = mid + 1;
+ 
+	// Merge the two parts into temp[].
+	while (i <= mid && j <= high)
+	{
+		if (a[i] < a[j])
+		{
+			temp[k] = a[i];
+			k++;
+			i++;
+		}
+		else
+		{
+			temp[k] = a[j];
+			k++;
+			j++;
+		}
+	}
+ 
+	// Insert all the remaining values from i to mid into temp[].
+	while (i <= mid)
+	{
+		temp[k] = a[i];
+		k++;
+		i++;
+	}
+ 
+	// Insert all the remaining values from j to high into temp[].
+	while (j <= high)
+	{
+		temp[k] = a[j];
+		k++;
+		j++;
+	}
+ 
+ 
+	// Assign sorted data stored in temp[] to a[].
+	for (i = low; i <= high; i++)
+	{
+		a[i] = temp[i-low];
+	}
 }
-
+ 
+// A function to split array into two parts.
+void MergeSort(int *a, int low, int high)
+{
+	int mid;
+	if (low < high)
+	{
+		mid=(low+high)/2;
+		// Split the data into two half.
+		MergeSort(a, low, mid);
+		MergeSort(a, mid+1, high);
+ 
+		// Merge them to get sorted output.
+		Merge(a, low, high, mid);
+	}
+}
+ 
 int main()
 {
-    int array[10] = {24, 15, 26, 45, 74, 12, 41, 32, 52, 42};
-    int n = 10;
-    for (auto ele : array)
-    {
-        cout << ele << " ";
-    }
-    return 0;
+	int n, i;
+	cout<<"\nEnter the number of data element to be sorted: ";
+	cin>>n;
+ 
+	int arr[n];
+	for(i = 0; i < n; i++)
+	{
+		cout<<"Enter element "<<i+1<<": ";
+		cin>>arr[i];
+	}
+ 
+	MergeSort(arr, 0, n-1);
+ 
+	// Printing the sorted data.
+	cout<<"\nSorted Data ";
+	for (i = 0; i < n; i++)
+        cout<<"->"<<arr[i];
+ 
+	return 0;
 }
